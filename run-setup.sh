490003f148dd42d34176f9b8a7ee0e27bb51e107
meljkohl <150753760+meljkohl@users.noreply.github.com>
2025-02-15 05:22:38 -0800

    Add conditionals for errors

--- run-setup.sh
+++ run-setup.sh
@@ -9,20 +9,40 @@ GITHUB_REPO=
 # ----------- Set Up GCP Project ----------- #
 gcloud config set project ${PROJECT_ID}
 
+if [ $? != 0 ]; then
+    echo "setting GCP project failed! did you set the PROJECT_ID bash variable?"
+    exit 1
+fi
+
 gcloud services enable run.googleapis.com \
     cloudbuild.googleapis.com
+
+if [ $? != 0 ]; then
+    echo "enabling cloud build & cloud run failed!"
+    exit 1
+fi
 
 gcloud projects add-iam-policy-binding ${PROJECT_ID} \
     --member=serviceAccount:${PROJECT_NUMBER}-compute@developer.gserviceaccount.com \
     --role=roles/run.builder
 
+if [ $? != 0 ]; then
+    echo "adding an iam policy binding failed!"
+    exit 1
+fi
+
 gcloud projects add-iam-policy-binding ${PROJECT_ID} \
     --member=serviceAccount:${PROJECT_NUMBER}-compute@developer.gserviceaccount.com \
     --role=roles/cloudbuild.builds.builder
 
+if [ $? != 0 ]; then
+    echo "adding an iam policy binding failed!"
+    exit 1
+fi
+
 # ---- Configure GCP for GitHub Actions ---- #
-OIDC_NAME=project-repo-test
-WORKLOAD_IDENTITY_POOL_NAME=github-test
+OIDC_NAME=github-project-repo
+WORKLOAD_IDENTITY_POOL_NAME=github-provider
 WORKLOAD_IDENTITY_POOL_ID="projects/${PROJECT_NUMBER}/locations/global/workloadIdentityPools/${WORKLOAD_IDENTITY_POOL_NAME}"
 WORKLOAD_IDENTITY_PROVIDER="projects/${PROJECT_NUMBER}/locations/global/workloadIdentityPools/${WORKLOAD_IDENTITY_POOL_NAME}/providers/${OIDC_NAME}"
 
@@ -30,6 +50,11 @@ gcloud iam workload-identity-pools create "${WORKLOAD_IDENTITY_POOL_NAME}" \
     --project="${PROJECT_ID}" \
     --location="global" \
     --display-name="GitHub Actions Pool"
+
+if [ $? != 0 ]; then
+    echo "creating a workload identity pool failed!"
+    exit 1
+fi
 
 gcloud iam workload-identity-pools providers create-oidc "${OIDC_NAME}" \
     --project="${PROJECT_ID}" \
@@ -40,10 +65,20 @@ gcloud iam workload-identity-pools providers create-oidc "${OIDC_NAME}" \
     --attribute-condition="assertion.repository_owner == '${GITHUB_ORG}'" \
     --issuer-uri="https://token.actions.githubusercontent.com"
 
+if [ $? != 0 ]; then
+    echo "creating an oidc failed!"
+    exit 1
+fi
+
 gcloud iam service-accounts add-iam-policy-binding "${PROJECT_NUMBER}-compute@developer.gserviceaccount.com" \
   --project="${PROJECT_ID}" \
   --role="roles/iam.workloadIdentityUser" \
   --member="principalSet://iam.googleapis.com/${WORKLOAD_IDENTITY_POOL_ID}/attribute.repository/${GITHUB_ORG}/${GITHUB_REPO_NAME}"
+
+if [ $? != 0 ]; then
+    echo "adding an iam policy binding failed!"
+    exit 1
+fi
 
 # ------- Output info for cloud.yml -------- #
 echo "
